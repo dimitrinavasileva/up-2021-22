2ee5e1bb75147ced750cceb6a97bb940841f778e
Dimitrina Vasileva <41293660+dimitrinavasileva@users.noreply.github.com>
2024-07-11 15:30:24 +0300

    Update BubbleSort.cpp

--- week-09/implementations/BubbleSort.cpp
+++ week-09/implementations/BubbleSort.cpp
@@ -3,19 +3,19 @@
 void bubbleSort(int* arr, int n)
 {
     for (int i = 0; i < n - 1; i++)
-	{
+    {
         bool swapped = false;
         for (int j = 0; j < n - i - 1; j++)
-		{
+	{
             if (arr[j] > arr[j + 1])
-			{
+	    {
                 swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
         }
 
         if (swapped == false)
-		{
+	{
             break;
         }
     }
@@ -23,13 +23,8 @@ void bubbleSort(int* arr, int n)
 
 void printArray(int* arr, int n)
 {
-    if (n > 0)
-	{
-		std::cout << arr[0] << " ";
-	}
-	
-    for (int i = 1; i < n; i++) 
-	{
+    for (int i = 0; i < n; i++) 
+    {
         std::cout << arr[i] << " ";
     }
 	
